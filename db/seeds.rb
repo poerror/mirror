c512d631cc71ce8aa60476dbc100d27376e05142
Jozef Pochyba <jozefpochyba@gmail.com>
2018-04-01 20:06:30 +0200

    Improve db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,16 +12,18 @@ project1 = Project.find_or_create_by(name: "INBOX", description: "Everything els
   project1_task1 = Task.find_or_create_by(
     name: "Fix table",
     description: "Finally fix the goddamn table",
-    status: :finished,
-    project: project1
+    status: :finished
   )
+  project1_task1.projects << project1 unless project1_task1.projects.include?(project1)
+  project1_task1.save
 
   # Simple short term task
   project1_task2 = Task.find_or_create_by(
     name: "Order a new lamp",
-    description: "Cannot handle dark anymore...",
-    project: project1
+    description: "Cannot handle dark anymore..."
   )
+  project1_task2.projects << project1 unless project1_task2.projects.include?(project1)
+  project1_task2.save
 
 project2 = Project.find_or_create_by(name: "Improve English", description: "Time to improve language")
 
@@ -29,29 +31,33 @@ project2 = Project.find_or_create_by(name: "Improve English", description: "Time
   project2_task1 = Task.find_or_create_by(
     name: "Every day check a video from https://www.engvid.com",
     description: "Title explains everything",
-    project: project2,
     longterm: true
   )
+  project2_task1.projects << project2 unless project2_task1.projects.include?(project2)
+  project2_task1.save
 
 project3 = Project.find_or_create_by(name: "Reading", description: "Time for reading books")
 
   # Task with multiple subtasks
   project3_task1 = Task.find_or_create_by(
     name: "Read 451 fahrenheit - Ray Bradbury",
-    description: "Read this awesome book",
-    project: project3
+    description: "Read this awesome book"
   )
+  project3_task1.projects << project3 unless project3_task1.projects.include?(project3)
+  project3_task1.save
 
     project3_task1_subtask1 = Task.find_or_create_by(
       name: "Read 451 fahrenheit - Ray Bradbury #1",
       description: "Finished on the page #39",
-      status: :finished,
-      parent_task: project3_task1
+      status: :finished
     )
+    project3_task1_subtask1.parent_task = project3_task1 unless project3_task1_subtask1.parent_task
+    project3_task1_subtask1.save
 
     project3_task1_subtask2 = Task.find_or_create_by(
-      name: "Read 451 fahrenheit - Ray Bradbury #1",
+      name: "Read 451 fahrenheit - Ray Bradbury #2",
       description: "Finished on the page #56",
-      status: :paused,
-      parent_task: project3_task1
+      status: :paused
     )
+    project3_task1_subtask2.parent_task = project3_task1 unless project3_task1_subtask2.parent_task
+    project3_task1_subtask2.save
